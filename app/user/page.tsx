a08041f86609eaddbf74d22ea8ff437115172ec0
Robin Sevelin <robin.sevelin@svenskaspel.se>
2024-04-15 10:36:12 +0200

    added logout

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -2,20 +2,28 @@
 
 import checkIfRedirect from "@/lib/urlProofing";
 import { User } from "@/pages/api/user";
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [user, setUser] = useState<User>();
+  const router = useRouter()
 
   useEffect(() => {
     setUser(JSON.parse(sessionStorage.getItem('user') || '{}'));
     checkIfRedirect('/user');
   }, []);
 
+  const logOut = () => {
+    sessionStorage.removeItem('user');
+    router.push('/')
+  }
+
   return (
     <>
       <section>
         <h1>Welcome {user?.name}</h1>
+        <button onClick={logOut}>Log out</button>
       </section>
     </>
   );
