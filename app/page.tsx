b31dbafc22a2ad377f3d245e28bc0799a94bd515
Robin Sevelin <robin.sevelin@svenskaspel.se>
2024-04-22 10:36:53 +0200

    added page routes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <Link className='btn-primary' href="/login">
+      <Link className='btn-primary' href="/pages/public/functions/login">
         Login
       </Link>
     </>
